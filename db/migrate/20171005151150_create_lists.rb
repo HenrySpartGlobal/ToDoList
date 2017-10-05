0a2d8060eda27a3f298f0c5fef61b277a61250fc
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-05 16:30:08 +0100

    basic set up

---
+++ db/migrate/20171005151150_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lists do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
